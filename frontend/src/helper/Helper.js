a9f5733678bb2e1ae46a4d17d2759726ea67e4ed
Sandeep Dara <148864994+sandeepdara-sd@users.noreply.github.com>
2025-03-04 10:00:11 +0530

    Update Helper.js

--- frontend/src/helper/Helper.js
+++ frontend/src/helper/Helper.js
@@ -1,2 +1,2 @@
-export const serverURL  = "https://blog-backend-j7af.onrender.com"
+export const serverURL  = "https://blog-app-joyv.onrender.com"
 //export const serverURL  = "http://localhost:5000"
