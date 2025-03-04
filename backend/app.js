943b9aff0e460132783206a950468b11bc95e7d0
Sandeep Dara <148864994+sandeepdara-sd@users.noreply.github.com>
2025-03-04 09:49:54 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,6 @@ app.use(express.json())
 app.use("/api/user", router)
 app.use("/api/blog",blogRouter)
 
-
 mongoose.connect("mongodb+srv://sandeepdara44:kmss1234@cluster0.ffdhqln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>app.listen(5000))
 .then(()=>console.log("DB Connected and listening on port 5000"))
